a5c387d85b441f8ed1487a5ade9909ceb00d0f65
Rodrigo de León <rodrigodeleonv@gmail.com>
2024-04-19 14:50:40 -0600

    feat: define reading loop process

--- door_access/rfid_reader.py
+++ door_access/rfid_reader.py
@@ -1,34 +1,85 @@
+from json.decoder import JSONDecodeError
 import logging
+
 from evdev import InputDevice, categorize, ecodes
 from evdev.events import InputEvent  # , KeyEvent
+import requests
 
 logger = logging.getLogger(__name__)
 
 
-def handle_key_press(data, rfid_tag: str):
-    if 2 <= data.scancode <= 11:
-        digit = str((data.scancode - 1) % 10)
-        rfid_tag += digit
-    elif data.scancode == 28:
-        logger.info(f"RFID Tag ID: {rfid_tag}")
-        rfid_tag = ""
-    return rfid_tag
+def handle_key_press(data: InputEvent) -> str:
+    """Converts data.scancode to a digit.
+
+    Digits: 0-9
+
+    Args:
+        data: InputEvent object representing a key press event.
+
+    Returns:
+        The last digit of the scancode.
+    """
+    digit = (data.scancode - 1) % 10
+    return str(digit)
 
 
-def main_loop(device_path: str):
+def send_post_request(session: requests.Session, url: str, tag_id: str) -> dict:
+    """Sends a POST request to the URL with the tag ID.
+
+    Args:
+        session: The requests.Session object.
+        url: The URL to send the POST request.
+        tag_id: The tag ID to send in the request body.
+
+    Returns:
+        A dictionary containing the` response data` and `status code`.
+    """
+    body = {"tag_id": tag_id}
+    data = {"status_code": None, "response": None}
+    try:
+        response = session.post(url, json=body)
+    except requests.exceptions.RequestException:
+        logger.exception("Failed to send POST request")
+        return
+    else:
+        data["status_code"] = response.status_code
+    try:
+        data["response"] = response.json()
+    except JSONDecodeError:
+        logger.exception("Error parsing response")
+        return
+    return data
+
+
+def main_loop(device_path: str, url: str):
+    rfid_tag = ""
+    session = requests.Session()
     reader = InputDevice(device_path)
     logger.info(f"Listening for RFID scans on device: {reader.path}")
-    rfid_tag = ""
 
     try:
+        # event: KeyEvent
         for event in reader.read_loop():
-            # event: KeyEvent
-            if event.type == ecodes.EV_KEY:
-                data: InputEvent = categorize(event)
-                if data.keystate == 1:
-                    rfid_tag = handle_key_press(data, rfid_tag)
+            if event.type != ecodes.EV_KEY:
+                # logger.debug(f"Skipping non-key event: {event}")
+                continue
+
+            data: InputEvent = categorize(event)
+
+            if data.keystate != 1:
+                # logger.debug(f"Skipping non-press event: {data}")
+                continue
+
+            if 2 <= data.scancode <= 11:
+                rfid_tag += handle_key_press(data)
+            elif data.scancode == 28:
+                logger.info(f"RFID Tag ID: {rfid_tag}")
+                logger.info("Sending POST request")
+                response_data = send_post_request(session, url, rfid_tag)
+                logger.info(f"Response: {response_data}")
+                rfid_tag = ""
     except KeyboardInterrupt:
-        logger.info("Exit from being reading")
+        logger.info("RFID service stopped")
     except Exception:
         logger.exception("An exception occurred and program will closed")
     finally:
--- mainproc.py
+++ mainproc.py
@@ -5,8 +5,19 @@ from door_access.rfid_reader import main_loop
 from door_access.find_reader import find_rfid_reader
 
 logger = logging.getLogger(__name__)
-logging_config()
 
-# Hardcoded name
-device = find_rfid_reader("Sycreader RFID Technology Co., Ltd SYC ID&IC USB Reader")
-main_loop(device.path)
+
+#
+# Hardcoded values
+DEV_NAME = ""
+URL = ""
+LOG_LEVEL = logging.DEBUG
+#
+
+logging_config(LOG_LEVEL)
+
+logger.info("Starting RDIFD reader")
+logger.info(f"Log level: {LOG_LEVEL}")
+
+device = find_rfid_reader(DEV_NAME)
+main_loop(device.path, URL)
