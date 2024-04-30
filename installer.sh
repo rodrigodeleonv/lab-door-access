16bf8baf591f4523f67342e034c6552536497078
Rodrigo de León <rodrigodeleonv@gmail.com>
2024-04-30 15:53:20 -0600

    chore: installer download link change the branch dev for main

--- installer.sh
+++ installer.sh
@@ -8,20 +8,20 @@ sudo mkdir -p $install_dir
 sudo $(which python) -m venv $install_dir/env
 
 sudo /opt/rfid-reader/env/bin/pip install -U pip setuptools wheel
-# sudo /opt/rfid-reader/env/bin/pip install -U lab-door-access
-#
-# TODO: remove. For test pypi can install dependencies
-sudo /opt/rfid-reader/env/bin/pip install -i https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple lab-door-access
-##
+sudo /opt/rfid-reader/env/bin/pip install -U lab-door-access
+# #
+# # TODO: remove. For test pypi can install dependencies
+# sudo /opt/rfid-reader/env/bin/pip install -i https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple lab-door-access
+# #
 
 sudo curl -o $install_dir/mainproc.py \
-    https://raw.githubusercontent.com/rodrigodeleonv/lab-door-access/dev/mainproc.py
+    https://raw.githubusercontent.com/rodrigodeleonv/lab-door-access/main/mainproc.py
 
 # Avoid alter existing config file: check if the file already exists
 if [ ! -f "$install_dir/config-reader.yml" ]; then
     # File doesn't exist, download it
     sudo curl -o $install_dir/config-reader.yml \
-        https://raw.githubusercontent.com/rodrigodeleonv/lab-door-access/dev/config-reader-example.yml
+        https://raw.githubusercontent.com/rodrigodeleonv/lab-door-access/main/config-reader-example.yml
 else
     echo "File config-reader.yml already exists. Skipping download."
 fi
@@ -30,7 +30,7 @@ sudo apt update
 sudo apt install supervisor -y
 
 sudo curl -o /etc/supervisor/conf.d/usb-reader.conf \
-    https://raw.githubusercontent.com/rodrigodeleonv/lab-door-access/dev/usb-reader.conf
+    https://raw.githubusercontent.com/rodrigodeleonv/lab-door-access/main/usb-reader.conf
 
 sudo supervisorctl reread
 sudo supervisorctl update
